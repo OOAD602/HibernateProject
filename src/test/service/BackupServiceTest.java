02c5ab339916a5e7ea44f80af821c5d9f3d2c6c7
sophiayj <okyjok@163.com>
2017-01-03 20:11:13 +0800

    update tests.

--- src/test/service/BackupServiceTest.java
+++ src/test/service/BackupServiceTest.java
@@ -1,4 +1,4 @@
-package test.service; 
+package test.service;
 
 import entity.Backup;
 import entity.BackupRecord;
@@ -11,7 +11,7 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Before; 
+import org.junit.Before;
 import org.junit.After;
 import service.BackupService;
 import service.EquipmentService;
@@ -24,12 +24,12 @@ import java.time.ZoneId;
 import java.util.List;
 
 /**
-* BackupService Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>一月 3, 2017</pre> 
-* @version 1.0 
-*/ 
+ * BackupService Tester.
+ *
+ * @author <Yiding Wen>
+ * @version 1.0
+ * @since <pre>一月 3, 2017</pre>
+ */
 public class BackupServiceTest {
     Configuration config = new Configuration();
     SessionFactory factory = config.configure().buildSessionFactory();
@@ -37,203 +37,191 @@ public class BackupServiceTest {
     LocalDate todayLocalDate = LocalDate.now(ZoneId.of("America/Montreal"));
     java.sql.Date sqlDate = java.sql.Date.valueOf(todayLocalDate);
 
-@Before
-public void before() throws Exception { 
-} 
-
-@After
-public void after() throws Exception { 
-} 
-
-/** 
-* 
-* Method: addBackup(String name, Role autho) 
-* 
-*/ 
-@Test
-public void testAddBackup() throws Exception {
-    BackupService bs = new BackupService();
-    Session session = factory.openSession();
-    bs.addBackup("test_add", Role.Admin);
-    Query query = session.createQuery( "select backupName,backPurchaseDate,backActive FROM Backup order by backupId desc");
-    query.setMaxResults(1);
-    //默认查询出来的list里存放的是一个Object数组
-    List<Object[]> list = query.list();
-    if(list.size()>0) {
-        String name = (String)list.get(0)[0];
-        Date date = (java.sql.Date)list.get(0)[1];
-        byte active = (byte)list.get(0)[2];
-        Assert.assertEquals("test_add", name);
-        Assert.assertEquals(sqlDate, date);
-        Assert.assertEquals(State.ACTIVE, active);
+    @Before
+    public void before() throws Exception {
     }
 
-    bs.addBackup("test_add2", Role.Purchaser);
-    Query query2 = session.createQuery( "select backupName,backPurchaseDate,backActive FROM Backup order by backupId desc");
-    query2.setMaxResults(1);
-    List<Object[]> list2 = query2.list();
-    if(list2.size()>0) {
-        String name2 = (String)list2.get(0)[0];
-        Date date2 = (java.sql.Date)list2.get(0)[1];
-        byte active2 = (byte)list2.get(0)[2];
-        Assert.assertEquals("test_add2", name2);
-        Assert.assertEquals(sqlDate, date2);
-        Assert.assertEquals(State.ACTIVE, active2);
+    @After
+    public void after() throws Exception {
     }
 
-    try {
-        bs.addBackup("test_add3", Role.HR);
-    } catch (AuthorityException e) {
-        Assert.assertTrue(e instanceof AuthorityException);
-    }
-    Query query3 = session.createQuery( "FROM Backup where backupName = :name");
-    query3.setParameter("name", "test_add3");
-    List<Backup> list3 = query3.list();
-    Assert.assertEquals(0,list3.size());
+    /**
+     * Method: addBackup(String name, Role autho)
+     */
+    @Test
+    public void testAddBackup() throws Exception {
+        BackupService bs = new BackupService();
+        Session session = factory.openSession();
+        bs.addBackup("test_add", Role.Admin);
+        Query query = session.createQuery("select backupName,backPurchaseDate,backActive FROM Backup order by backupId desc");
+        query.setMaxResults(1);
+        //默认查询出来的list里存放的是一个Object数组
+        List<Object[]> list = query.list();
+        if (list.size() > 0) {
+            String name = (String) list.get(0)[0];
+            Date date = (java.sql.Date) list.get(0)[1];
+            byte active = (byte) list.get(0)[2];
+            Assert.assertEquals("test_add", name);
+            Assert.assertEquals(sqlDate, date);
+            Assert.assertEquals(State.ACTIVE, active);
+        }
 
-    try {
-        bs.addBackup("test_add4", Role.Employee);
-    } catch (AuthorityException e) {
-        Assert.assertTrue(e instanceof AuthorityException);
-    }
-    Query query4 = session.createQuery( "FROM Backup where backupName = :name");
-    query4.setParameter("name", "test_add4");
-    List<Backup> list4 = query4.list();
-    Assert.assertEquals(0,list4.size());
+        bs.addBackup("test_add2", Role.Purchaser);
+        Query query2 = session.createQuery("select backupName,backPurchaseDate,backActive FROM Backup order by backupId desc");
+        query2.setMaxResults(1);
+        List<Object[]> list2 = query2.list();
+        if (list2.size() > 0) {
+            String name2 = (String) list2.get(0)[0];
+            Date date2 = (java.sql.Date) list2.get(0)[1];
+            byte active2 = (byte) list2.get(0)[2];
+            Assert.assertEquals("test_add2", name2);
+            Assert.assertEquals(sqlDate, date2);
+            Assert.assertEquals(State.ACTIVE, active2);
+        }
+
+        try {
+            bs.addBackup("test_add3", Role.HR);
+        } catch (AuthorityException e) {
+            Assert.assertTrue(e instanceof AuthorityException);
+        }
+        Query query3 = session.createQuery("FROM Backup where backupName = :name");
+        query3.setParameter("name", "test_add3");
+        List<Backup> list3 = query3.list();
+        Assert.assertEquals(0, list3.size());
+
+        try {
+            bs.addBackup("test_add4", Role.Employee);
+        } catch (AuthorityException e) {
+            Assert.assertTrue(e instanceof AuthorityException);
+        }
+        Query query4 = session.createQuery("FROM Backup where backupName = :name");
+        query4.setParameter("name", "test_add4");
+        List<Backup> list4 = query4.list();
+        Assert.assertEquals(0, list4.size());
 
 
-    session.close();
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: brokeBackup(String backupId, Role autho) 
-* 
-*/ 
-@Test
-public void testBrokeBackup() throws Exception { 
-//TODO: Test goes here...
-    BackupService bs = new BackupService();
-    String bId = bs.addBackup("test_broke", Role.Admin);
-    String bId2 = bs.addBackup("test_broke2", Role.Admin);
-    String bId3 = bs.addBackup("test_broke3", Role.Admin);
-    String bId4 = bs.addBackup("test_broke4", Role.Admin);
-
-    bs.brokeBackup(bId, Role.Admin);
-    Session session = factory.openSession();
-    Query query = session.createQuery( "FROM Backup where backupId = :id");
-    query.setMaxResults(1);
-    query.setParameter("id", bId);
-    List<Backup> list = query.list();
-    if(list.size()>0) {
-        Assert.assertEquals(sqlDate, list.get(0).getBackBrokenDate());
-    }
-
-    try {
-        bs.brokeBackup(bId2, Role.Employee);
-    } catch (AuthorityException e) {
-        Assert.assertTrue(e instanceof AuthorityException);
-    }
-    Query query2 = session.createQuery( "FROM Backup where backupId = :id");
-    query2.setMaxResults(1);
-    query2.setParameter("id", bId2);
-    List<Backup> list2 = query2.list();
-    Assert.assertEquals(1,list2.size());
-    if(list2.size()>0) {
-        Assert.assertEquals(null, list2.get(0).getBackBrokenDate());
+        session.close();
     }
 
-    try {
-        bs.brokeBackup(bId3, Role.HR);
-    } catch (AuthorityException e) {
-        Assert.assertTrue(e instanceof AuthorityException);
-    }
-    Query query3 = session.createQuery( "FROM Backup where backupId = :id");
-    query3.setMaxResults(1);
-    query3.setParameter("id", bId3);
-    List<Backup> list3 = query3.list();
-    Assert.assertEquals(1,list3.size());
-    if(list3.size()>0) {
-        Assert.assertEquals(null, list3.get(0).getBackBrokenDate());
+    /**
+     * Method: brokeBackup(String backupId, Role autho)
+     */
+    @Test
+    public void testBrokeBackup() throws Exception {
+        BackupService bs = new BackupService();
+        String bId = bs.addBackup("test_broke", Role.Admin);
+        String bId2 = bs.addBackup("test_broke2", Role.Admin);
+        String bId3 = bs.addBackup("test_broke3", Role.Admin);
+        String bId4 = bs.addBackup("test_broke4", Role.Admin);
+
+        bs.brokeBackup(bId, Role.Admin);
+        Session session = factory.openSession();
+        Query query = session.createQuery("FROM Backup where backupId = :id");
+        query.setMaxResults(1);
+        query.setParameter("id", bId);
+        List<Backup> list = query.list();
+        if (list.size() > 0) {
+            Assert.assertEquals(sqlDate, list.get(0).getBackBrokenDate());
+        }
+
+        try {
+            bs.brokeBackup(bId2, Role.Employee);
+        } catch (AuthorityException e) {
+            Assert.assertTrue(e instanceof AuthorityException);
+        }
+        Query query2 = session.createQuery("FROM Backup where backupId = :id");
+        query2.setMaxResults(1);
+        query2.setParameter("id", bId2);
+        List<Backup> list2 = query2.list();
+        Assert.assertEquals(1, list2.size());
+        if (list2.size() > 0) {
+            Assert.assertEquals(null, list2.get(0).getBackBrokenDate());
+        }
+
+        try {
+            bs.brokeBackup(bId3, Role.HR);
+        } catch (AuthorityException e) {
+            Assert.assertTrue(e instanceof AuthorityException);
+        }
+        Query query3 = session.createQuery("FROM Backup where backupId = :id");
+        query3.setMaxResults(1);
+        query3.setParameter("id", bId3);
+        List<Backup> list3 = query3.list();
+        Assert.assertEquals(1, list3.size());
+        if (list3.size() > 0) {
+            Assert.assertEquals(null, list3.get(0).getBackBrokenDate());
+        }
+
+        bs.brokeBackup(bId4, Role.Purchaser);
+        Query query4 = session.createQuery("FROM Backup where backupId = :id");
+        query4.setMaxResults(1);
+        query4.setParameter("id", bId4);
+        List<Backup> list4 = query4.list();
+        Assert.assertEquals(1, list4.size());
+        if (list4.size() > 0) {
+            Assert.assertEquals(sqlDate, list4.get(0).getBackBrokenDate());
+        }
+
+        session.close();
     }
 
-    bs.brokeBackup(bId4, Role.Purchaser);
-    Query query4 = session.createQuery( "FROM Backup where backupId = :id");
-    query4.setMaxResults(1);
-    query4.setParameter("id", bId4);
-    List<Backup> list4 = query4.list();
-    Assert.assertEquals(1,list4.size());
-    if(list4.size()>0) {
-        Assert.assertEquals(sqlDate, list4.get(0).getBackBrokenDate());
+    /**
+     * Method: borrowBackup(String employeeId, String eId, String backupName)
+     */
+    @Test
+    public void testBorrowBackup() throws Exception {
+        BackupService bs = new BackupService();
+        bs.addBackup("test_borrow", Role.Admin);
+        String bId = bs.borrowBackup("U001", eId, "test_borrow");
+        Session session = factory.openSession();
+        Query query = session.createQuery("FROM Backup where backupId = :id");
+        query.setMaxResults(1);
+        query.setParameter("id", bId);
+        //默认查询出来的list里存放的是一个Object数组
+        List<Backup> list = query.list();
+        if (list.size() > 0) {
+            Assert.assertEquals(State.INACTIVE, (byte) list.get(0).getBackActive());
+        }
+        Query queryRecord = session.createQuery("FROM BackupRecord where backupId = :id order by backupRecordId desc");
+        queryRecord.setMaxResults(1);
+        queryRecord.setParameter("id", bId);
+        List<BackupRecord> recordList = queryRecord.list();
+        if (recordList.size() > 0) {
+            Assert.assertEquals("U001", recordList.get(0).getEmployeeId());
+            Assert.assertEquals(eId, recordList.get(0).getEquipmentId());
+            LocalDate todayLocalDate = LocalDate.now(ZoneId.of("America/Montreal"));
+            java.sql.Date sqlDate = java.sql.Date.valueOf(todayLocalDate);
+            Assert.assertEquals(sqlDate, recordList.get(0).getBackApplyDate());
+            Assert.assertEquals(null, recordList.get(0).getBackReturnDate());
+        }
     }
 
-    session.close();
-} 
-
-/** 
-* 
-* Method: borrowBackup(String employeeId, String eId, String backupName) 
-* 
-*/ 
-@Test
-public void testBorrowBackup() throws Exception { 
-//TODO: Test goes here...
-    BackupService bs = new BackupService();
-    bs.addBackup("test_borrow", Role.Admin);
-    String bId = bs.borrowBackup("U001", eId,"test_borrow");
-    Session session = factory.openSession();
-    Query query = session.createQuery( "FROM Backup where backupId = :id");
-    query.setMaxResults(1);
-    query.setParameter("id", bId);
-    //默认查询出来的list里存放的是一个Object数组
-    List<Backup> list = query.list();
-    if(list.size()>0) {
-        Assert.assertEquals(State.INACTIVE, (byte)list.get(0).getBackActive());
-    }
-    Query queryRecord = session.createQuery("FROM BackupRecord where backupId = :id order by backupRecordId desc");
-    queryRecord.setMaxResults(1);
-    queryRecord.setParameter("id", bId );
-    List<BackupRecord> recordList = queryRecord.list();
-    if(recordList.size()>0) {
-        Assert.assertEquals("U001", recordList.get(0).getEmployeeId());
-        Assert.assertEquals(eId, recordList.get(0).getEquipmentId());
+    /**
+     * Method: returnBackup(String backupId)
+     */
+    @Test
+    public void testReturnBackup() throws Exception {
+        BackupService bs = new BackupService();
+        bs.addBackup("test_return", Role.Admin);
+        String bId = bs.borrowBackup("U001", eId, "test_return");
+        bs.returnBackup(bId);
+        Session session = factory.openSession();
+        Query query = session.createQuery("FROM Backup where backupId = :id");
+        query.setMaxResults(1);
+        query.setParameter("id", bId);
+        List<Backup> list = query.list();
+        Assert.assertEquals(1, list.size());
+        if (list.size() > 0) {
+            Assert.assertEquals(State.ACTIVE, (byte) list.get(0).getBackActive());
+        }
+        Query queryRecord = session.createQuery("FROM BackupRecord order by backupRecordId desc");
+        queryRecord.setMaxResults(1);
+        List<BackupRecord> recordList = queryRecord.list();
         LocalDate todayLocalDate = LocalDate.now(ZoneId.of("America/Montreal"));
         java.sql.Date sqlDate = java.sql.Date.valueOf(todayLocalDate);
-        Assert.assertEquals(sqlDate, recordList.get(0).getBackApplyDate());
-        Assert.assertEquals(null, recordList.get(0).getBackReturnDate());
+        Assert.assertEquals(sqlDate, recordList.get(0).getBackReturnDate());
+        session.close();
     }
-} 
-
-/** 
-* 
-* Method: returnBackup(String backupId) 
-* 
-*/ 
-@Test
-public void testReturnBackup() throws Exception { 
-//TODO: Test goes here...
-    BackupService bs = new BackupService();
-    bs.addBackup("test_return", Role.Admin);
-    String bId = bs.borrowBackup("U001", eId,"test_return");
-    bs.returnBackup(bId);
-    Session session = factory.openSession();
-    Query query = session.createQuery( "FROM Backup where backupId = :id");
-    query.setMaxResults(1);
-    query.setParameter("id", bId);
-    List<Backup> list = query.list();
-    Assert.assertEquals(1,list.size());
-    if(list.size()>0) {
-        Assert.assertEquals(State.ACTIVE, (byte)list.get(0).getBackActive());
-    }
-    Query queryRecord = session.createQuery("FROM BackupRecord order by backupRecordId desc");
-    queryRecord.setMaxResults(1);
-    List<BackupRecord> recordList = queryRecord.list();
-    LocalDate todayLocalDate = LocalDate.now(ZoneId.of("America/Montreal"));
-    java.sql.Date sqlDate = java.sql.Date.valueOf(todayLocalDate);
-    Assert.assertEquals(sqlDate, recordList.get(0).getBackReturnDate());
-    session.close();
-} 
 
 
 } 
--- src/test/service/SearchServiceTest.java
+++ src/test/service/SearchServiceTest.java
@@ -1,165 +1,258 @@
 package test.service;
 
+import entity.*;
+import exception.AuthorityException;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+import service.Role;
+import service.SearchService;
+import service.State;
+
+import java.util.List;
 
 /**
-* SearchService Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>一月 3, 2017</pre> 
-* @version 1.0 
-*/ 
-public class SearchServiceTest { 
+ * SearchService Tester.
+ *
+ * @author <Jun Yuan>
+ * @version 1.0
+ * @since <pre>一月 3, 2017</pre>
+ */
+public class SearchServiceTest {
+    @Before
+    public void before() throws Exception {
+    }
 
-@Before
-public void before() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    /**
+     * Method: getAllMyEquipment(String userId)
+     */
+    @Test
+    public void testGetAllMyEquipment() throws Exception {
+        SearchService ss = new SearchService();
+        String userId = "employeeAAA";
+        List<Equipment> list = ss.getAllMyEquipment(userId);
 
-/** 
-* 
-* Method: getAllMyEquipment(String userId) 
-* 
-*/ 
-@Test
-public void testGetAllMyEquipment() throws Exception { 
-//TODO: Test goes here... 
-} 
+        Equipment expected = new Equipment();
+        expected.setEquipmentId("a");
+        expected.setEquipmentName("Mac");
+        expected.setEquipmentActive(State.INACTIVE);
+        expected.setPurchaseDate(null);
+        expected.setBrokenDate(null);
 
-/** 
-* 
-* Method: allMyLog(String userId) 
-* 
-*/ 
-@Test
-public void testAllMyLog() throws Exception { 
-//TODO: Test goes here... 
-} 
+        Assert.assertEquals(expected, list.get(0));
+    }
 
-/** 
-* 
-* Method: allEquipment(Role role) 
-* 
-*/ 
-@Test
-public void testAllEquipment() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+     * Method: allMyLog(String userId)
+     */
+    @Test
+    public void testAllMyLog() throws Exception {
+        SearchService ss = new SearchService();
+        String userId = "ea";
+        List list = ss.allMyLog(userId);
 
-/** 
-* 
-* Method: allBackup(Role role) 
-* 
-*/ 
-@Test
-public void testAllBackup() throws Exception { 
-//TODO: Test goes here... 
-} 
+        Assert.assertEquals(list.size(), 2);
+        Assert.assertTrue(list.get(0) instanceof BackupRecord);
+        Assert.assertTrue(list.get(1) instanceof SoftwareRecord);
+    }
 
-/** 
-* 
-* Method: equipmentLog(Role role) 
-* 
-*/ 
-@Test
-public void testEquipmentLog() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+     * Method: allEquipment(Role role)
+     */
+    @Test
+    public void testAllEquipment() throws Exception {
+        SearchService ss = new SearchService();
+        Role role = Role.Admin;
+        List<Equipment> list = ss.allEquipment(role);
 
-/** 
-* 
-* Method: listSoftOwners(String softId, Role role) 
-* 
-*/ 
-@Test
-public void testListSoftOwners() throws Exception { 
-//TODO: Test goes here... 
-} 
+        Assert.assertEquals(3, list.size());
 
-/** 
-* 
-* Method: listEquipmentOwners(String equipmentId, Role role) 
-* 
-*/ 
-@Test
-public void testListEquipmentOwners() throws Exception { 
-//TODO: Test goes here... 
-} 
+        role = Role.Purchaser;
+        Exception exception = null;
+        try {
+            ss.allEquipment(role);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertTrue(exception instanceof AuthorityException);
+    }
 
-/** 
-* 
-* Method: listEquipmentBackupLog(String equipmentId, Role role) 
-* 
-*/ 
-@Test
-public void testListEquipmentBackupLog() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+     * Method: allBackup(Role role)
+     */
+    @Test
+    public void testAllBackup() throws Exception {
+        SearchService ss = new SearchService();
+        Role role = Role.Admin;
+        List<Backup> list = ss.allBackup(role);
 
-/** 
-* 
-* Method: listUsableEquipmentId(String equipmentName, Role role) 
-* 
-*/ 
-@Test
-public void testListUsableEquipmentId() throws Exception { 
-//TODO: Test goes here... 
-} 
+        Assert.assertEquals(2, list.size());
+        role = Role.Purchaser;
+        Exception exception = null;
+        try {
+            ss.allBackup(role);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertTrue(exception instanceof AuthorityException);
+    }
 
-/** 
-* 
-* Method: listUsableSoftId(String softName, Role role) 
-* 
-*/ 
-@Test
-public void testListUsableSoftId() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+     * Method: equipmentLog(Role role)
+     */
+    @Test
+    public void testEquipmentLog() throws Exception {
+        SearchService ss = new SearchService();
+        Role role = Role.Admin;
+        List<EquipmentRecord> list = ss.equipmentLog(role);
 
-/** 
-* 
-* Method: listUsableBackupId(String backupName, Role role) 
-* 
-*/ 
-@Test
-public void testListUsableBackupId() throws Exception { 
-//TODO: Test goes here... 
-} 
+        Assert.assertEquals(1, list.size());
+        role = Role.Purchaser;
+        Exception exception = null;
+        try {
+            ss.allBackup(role);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertTrue(exception instanceof AuthorityException);
+    }
 
-/** 
-* 
-* Method: listBackupLog(String backupId, Role role) 
-* 
-*/ 
-@Test
-public void testListBackupLog() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+     * Method: listSoftOwners(String softId, Role role)
+     */
+    @Test
+    public void testListSoftOwners() throws Exception {
+        SearchService ss = new SearchService();
+        Role role = Role.Admin;
+        String softId = "sb";
+        List<String> list = ss.listSoftOwners(softId, role);
 
-/** 
-* 
-* Method: listUsersAll(String userId, Role role) 
-* 
-*/ 
-@Test
-public void testListUsersAll() throws Exception { 
-//TODO: Test goes here... 
-} 
+        Assert.assertEquals(list.get(0), "ea");
+        role = Role.Purchaser;
+        Exception exception = null;
+        try {
+            ss.listSoftOwners(softId, role);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertTrue(exception instanceof AuthorityException);
+    }
 
-/** 
-* 
-* Method: listUsersLog(String userId, Role role) 
-* 
-*/ 
-@Test
-public void testListUsersLog() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+     * Method: listEquipmentOwners(String equipmentId, Role role)
+     */
+    @Test
+    public void testListEquipmentOwners() throws Exception {
+        SearchService ss = new SearchService();
+        Role role = Role.Admin;
+        String equipmentId = "a";
+        List<String> list = ss.listEquipmentOwners(equipmentId, role);
 
+        Assert.assertEquals(list.size(), 1);
+        Assert.assertEquals(list.get(0), "employeeAAA");
+        role = Role.Purchaser;
+        Exception exception = null;
+        try {
+            ss.listEquipmentOwners(equipmentId, role);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertTrue(exception instanceof AuthorityException);
+    }
+
+    /**
+     * Method: listEquipmentBackupLog(String equipmentId, Role role)
+     */
+    @Test
+    public void testListEquipmentBackupLog() throws Exception {
+        SearchService ss = new SearchService();
+        Role role = Role.Admin;
+        String eqId = "a";
+        List<BackupRecord> list = ss.listEquipmentBackupLog(eqId, role);
+
+        Assert.assertEquals(list.size(), 1);
+        Assert.assertEquals(list.get(0).getBackupId(), "bb");
+        role = Role.Purchaser;
+        Exception exception = null;
+        try {
+            ss.listEquipmentOwners(eqId, role);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertTrue(exception instanceof AuthorityException);
+    }
+
+    /**
+     * Method: listUsableEquipmentId(String equipmentName, Role role)
+     */
+    @Test
+    public void testListUsableEquipmentId() throws Exception {
+        SearchService ss = new SearchService();
+        Role role = Role.Admin;
+        List<String> list = ss.listUsableEquipmentId("Mac", role);
+
+        Assert.assertEquals(list.size(), 2);
+        role = Role.Purchaser;
+        Exception exception = null;
+        try {
+            ss.listUsableEquipmentId("Mac", role);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertTrue(exception instanceof AuthorityException);
+    }
+
+    /**
+     * Method: listUsersAll(String userId, Role role)
+     */
+    @Test
+    public void testListUsersAll() throws Exception {
+        SearchService ss = new SearchService();
+        String userId = "ea";
+        Role role = Role.Admin;
+        List list = ss.listUsersAll(userId, role);
+
+        Assert.assertEquals(2, list.size());
+        Assert.assertTrue(list.get(0) instanceof Backup);
+        Assert.assertTrue(list.get(1) instanceof Software);
+
+        role = Role.Employee;
+        Exception exception = null;
+        try {
+            ss.listUsersAll(userId, role);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertTrue(exception instanceof AuthorityException);
+    }
+
+    /**
+     * Method: listUsersLog(String userId, Role role)
+     */
+    @Test
+    public void testListUsersLog() throws Exception {
+        SearchService ss = new SearchService();
+        String userId = "ea";
+        Role role = Role.Admin;
+        List list = ss.listUsersLog(userId, role);
+
+        Assert.assertEquals(2, list.size());
+        Assert.assertTrue(list.get(0) instanceof BackupRecord);
+        Assert.assertTrue(list.get(1) instanceof SoftwareRecord);
+
+        role = Role.Employee;
+        Exception exception = null;
+        try {
+            ss.listUsersLog(userId, role);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertTrue(exception instanceof AuthorityException);
+    }
 
 } 
