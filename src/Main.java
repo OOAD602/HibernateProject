4b4eed97830bf0fd0cbe24e7eb4b7b3db08a3301
sophiayj <okyjok@163.com>
2017-01-03 13:54:39 +0800

    merge tests

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,7 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-<<<<<<< HEAD
-import service.BackupService;
-import service.EquipmentService;
-import service.Role;
-import service.SoftwareService;
-=======
 import service.*;
->>>>>>> c7073ae6cb31b5bbcb324cd86c555e59c0af88b0
 
 import java.util.List;
 import java.util.Scanner;
@@ -40,7 +33,6 @@ public class Main {
         EquipmentService es = new EquipmentService();
         BackupService bs = new BackupService();
         SoftwareService ss = new SoftwareService();
-<<<<<<< HEAD
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入用户类型：");
         String usertype = sc.next();
@@ -65,142 +57,136 @@ public class Main {
         String userId = sc.next();
         System.out.println("请输入操作类别：");
 
-        while(sc.hasNext()) {
-=======
-        SearchService schs = new SearchService();
-
-        //        es.addEquipment("macbookpro", 0);
-//        es.borrowEquipment("U001","macbookpro");
-        Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
->>>>>>> c7073ae6cb31b5bbcb324cd86c555e59c0af88b0
-            String opr = sc.next();
-            switch (opr) {
-                case "adde":
-                    System.out.println("请输入新购买设备的名称");
-                    String newEName = sc.next();
-                    es.addEquipment(newEName, role);
-                    break;
-                case "addb":
-                    System.out.println("请输入新购买备件的名称");
-                    String newBName = sc.next();
-                    bs.addBackup(newBName, role);
-                    break;
-                case "adds":
-                    System.out.println("请输入新购买软件的名称");
-                    String newSName = sc.next();
-                    ss.addSoftware(newSName, role);
-                    break;
-                case "be":
-                    System.out.println("请输入申请设备的名称");
-                    String eName = sc.next();
-                    es.borrowEquipment(userId, eName);
-                    break;
-                case "re":
-                    System.out.println("请输入归还设备的ID");
-                    String eId = sc.next();
-                    es.returnEquipment(eId);
-                    break;
-                case "bb":
-                    System.out.println("请输入申请备件的名称");
-                    String bName = sc.next();
-                    System.out.println("请输入安装备件的设备的ID");
-                    String addEId = sc.next();
-<<<<<<< HEAD
-                    bs.borrowBackup(userId,addEId, bName);
-=======
-                    bs.borrowBackup("U001", addEId, bName);
->>>>>>> c7073ae6cb31b5bbcb324cd86c555e59c0af88b0
-                    break;
-                case "rb":
-                    System.out.println("请输入归还备件的ID");
-                    String rbId = sc.next();
-                    bs.returnBackup(rbId);
-                    break;
-                case "is":
-                    System.out.println("请输入申请软件的名称");
-                    String isName = sc.next();
-                    ss.installSoftware(userId, isName);
-                    break;
-                case "us":
-                    System.out.println("请输入归还软件的ID");
-                    String isId = sc.next();
-                    ss.uninstallSoftware(isId);
-                    break;
-                case "search":
-                    System.out.println("请输入需要查询的对象：");
-                    opr = sc.next();
-                    String id;
-                    String name;
-                    Role role = Role.Admin;
-                    List<?> result = null;
-                    switch (opr) {
-                        case "eqbyuid":
-                            id = sc.next();
-                            result = schs.getAllMyEquipment(id);
-                            break;
-                        case "mylogbyuid":
-                            id = sc.next();
-                            result = schs.allMyLog(id);
-                            break;
-                        case "alleq":
-                            result = schs.allEquipment(role);
-                            break;
-                        case "allbc":
-                            result = schs.allBackup(role);
-                            break;
-                        case "alleqr":
-                            result = schs.equipmentLog(role);
-                            break;
-                        case "ubysid":
-                            id = sc.next();
-                            result = schs.listSoftOwners(id, role);
-                            break;
-                        case "ubyeid":
-                            id = sc.next();
-                            result = schs.listEquipmentOwners(id, role);
-                            break;
-                        case "bcrbyeid":
-                            id = sc.next();
-                            result = schs.listEquipmentBackupLog(id, role);
-                            break;
-                        case "eidbyname":
-                            name = sc.next();
-                            result = schs.listUsableEquipmentId(name, role);
-                            break;
-                        case "sidbyname":
-                            name = sc.next();
-                            result = schs.listUsableSoftId(name, role);
-                            break;
-                        case "bidbyname":
-                            name = sc.next();
-                            result = schs.listUsableBackupId(name, role);
-                            break;
-                        case "bcrbybid":
-                            id = sc.next();
-                            result = schs.listBackupLog(id, role);
-                            break;
-                        case "allbyuid":
-                            id = sc.next();
-                            result = schs.listUsersAll(id, role);
-                            break;
-                        case "logbyuid":
-                            id = sc.next();
-                            result = schs.listUsersLog(id, role);
-                            break;
-                        default:
-                            System.out.println("请输入正确的指令");
-                    }
-                    if (result != null){
-                        if (result.isEmpty()) {
-                            System.out.println("No Result!");
-                        } else {
-                            iterateOutput(result);
+            SearchService schs = new SearchService();
+
+            //        es.addEquipment("macbookpro", 0);
+//        es.borrowEquipment("U001","macbookpro");
+            while (sc.hasNext()) {
+                String opr = sc.next();
+                switch (opr) {
+                    case "adde":
+                        System.out.println("请输入新购买设备的名称");
+                        String newEName = sc.next();
+                        es.addEquipment(newEName, role);
+                        break;
+                    case "addb":
+                        System.out.println("请输入新购买备件的名称");
+                        String newBName = sc.next();
+                        bs.addBackup(newBName, role);
+                        break;
+                    case "adds":
+                        System.out.println("请输入新购买软件的名称");
+                        String newSName = sc.next();
+                        ss.addSoftware(newSName, role);
+                        break;
+                    case "be":
+                        System.out.println("请输入申请设备的名称");
+                        String eName = sc.next();
+                        es.borrowEquipment(userId, eName);
+                        break;
+                    case "re":
+                        System.out.println("请输入归还设备的ID");
+                        String eId = sc.next();
+                        es.returnEquipment(eId);
+                        break;
+                    case "bb":
+                        System.out.println("请输入申请备件的名称");
+                        String bName = sc.next();
+                        System.out.println("请输入安装备件的设备的ID");
+                        String addEId = sc.next();
+                        bs.borrowBackup(userId, addEId, bName);
+                        break;
+                    case "rb":
+                        System.out.println("请输入归还备件的ID");
+                        String rbId = sc.next();
+                        bs.returnBackup(rbId);
+                        break;
+                    case "is":
+                        System.out.println("请输入申请软件的名称");
+                        String isName = sc.next();
+                        ss.installSoftware(userId, isName);
+                        break;
+                    case "us":
+                        System.out.println("请输入归还软件的ID");
+                        String isId = sc.next();
+                        ss.uninstallSoftware(isId);
+                        break;
+                    case "search":
+                        System.out.println("请输入需要查询的对象：");
+                        opr = sc.next();
+                        String id;
+                        String name;
+                        role = Role.Admin;
+                        List<?> result = null;
+                        switch (opr) {
+                            case "eqbyuid":
+                                id = sc.next();
+                                result = schs.getAllMyEquipment(id);
+                                break;
+                            case "mylogbyuid":
+                                id = sc.next();
+                                result = schs.allMyLog(id);
+                                break;
+                            case "alleq":
+                                result = schs.allEquipment(role);
+                                break;
+                            case "allbc":
+                                result = schs.allBackup(role);
+                                break;
+                            case "alleqr":
+                                result = schs.equipmentLog(role);
+                                break;
+                            case "ubysid":
+                                id = sc.next();
+                                result = schs.listSoftOwners(id, role);
+                                break;
+                            case "ubyeid":
+                                id = sc.next();
+                                result = schs.listEquipmentOwners(id, role);
+                                break;
+                            case "bcrbyeid":
+                                id = sc.next();
+                                result = schs.listEquipmentBackupLog(id, role);
+                                break;
+                            case "eidbyname":
+                                name = sc.next();
+                                result = schs.listUsableEquipmentId(name, role);
+                                break;
+                            case "sidbyname":
+                                name = sc.next();
+                                result = schs.listUsableSoftId(name, role);
+                                break;
+                            case "bidbyname":
+                                name = sc.next();
+                                result = schs.listUsableBackupId(name, role);
+                                break;
+                            case "bcrbybid":
+                                id = sc.next();
+                                result = schs.listBackupLog(id, role);
+                                break;
+                            case "allbyuid":
+                                id = sc.next();
+                                result = schs.listUsersAll(id, role);
+                                break;
+                            case "logbyuid":
+                                id = sc.next();
+                                result = schs.listUsersLog(id, role);
+                                break;
+                            default:
+                                System.out.println("请输入正确的指令");
                         }
-                    }
-                    break;
-                default:
-                    System.out.println("请输入正确的指令");
+                        if (result != null) {
+                            if (result.isEmpty()) {
+                                System.out.println("No Result!");
+                            } else {
+                                iterateOutput(result);
+                            }
+                        }
+                        break;
+                    default:
+                        System.out.println("请输入正确的指令");
+                }
             }
         }
     }
